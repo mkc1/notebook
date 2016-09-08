780f23309e545eb1825a0f2eb3fcd00258da8723
mkc1 <coulism@gmail.com>
2016-09-08 12:47:23 -0400

    working edit and delete buttons

--- notes/urls.py
+++ notes/urls.py
@@ -6,5 +6,7 @@ app_name = 'notes'
 urlpatterns = [
   url(r'^$', views.IndexView.as_view(), name='index'),
   url(r'^(?P<pk>[0-9]+)$', views.DetailView.as_view(), name='detail'),
-  url(r'add/$', views.NoteCreate.as_view(), name="note-add")
+  url(r'add/$', views.NoteCreate.as_view(), name="note-add"),
+  url(r'/(?P<pk>[0-9]+)$', views.NoteUpdate.as_view(), name="note-update"),
+  url(r'(?P<pk>[0-9]+)/delete/$', views.NoteDelete.as_view(), name="note-delete")
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 from .models import Note
 
 class IndexView(generic.ListView):
@@ -14,3 +15,12 @@ class DetailView(generic.DetailView):
 class NoteCreate(CreateView):
   model = Note
   fields = ['note_title', 'body']
+
+class NoteUpdate(UpdateView):
+  model = Note
+  fields = ['note_title', 'body']
+
+class NoteDelete(DeleteView):
+  model = Note
+  # redirect to homepage
+  success_url = reverse_lazy('notes:index')
